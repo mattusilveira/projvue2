d62234b728ef530ddbf4b78db1c87cd25a639068
mattusilveira <matheus12062007silveira7@gmail.com>
2024-03-13 11:47:25 -0300

    proj2

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const listaCidades = [
+  'São Paulo',
+  'Rio de Janeiro',
+  'Belo Horizonte',
+  'Salvador',
+  'Fortaleza',
+  'Curitiba',
+  'Manaus',
+  'Recife',
+  'Porto Alegre',
+  'Brasília',
+];
+
+</script>
+<template>
+  <ul>
+    <li v-for="(value, key) in listaCidades.sort()" :key="key">{{ key }}: {{ value }}</li>
+  </ul>
+</template>
+<style>
+ul {
+  font-size: 35px;
+
+}
+</style>
